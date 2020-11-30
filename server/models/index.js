233c1c619461bf85221c2aae2d5260a2ee2544ee
cooperama <coope133@gmail.com>
2020-11-30 09:42:01 -0800

    Connects to mongo

---
+++ server/models/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/codependent";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+mongoose
+  .connect(connectionString, configOptions)
+  .then(() => console.log("MongoDB successfully connected~~"))
+  .catch((err) => console.log("MongoDB connection error: ", err));
+
+module.exports = {
+  User: require("./User"),
+  Codegory: require("./Codegory"),
+  Comment: require("./Comment"),
+  Room: require("./Room"),
+};
