1c6bf9d0cb05ac68789fec6537a01aedda44ee2c
cooperama <coope133@gmail.com>
2020-11-30 16:42:09 -0800

    Adds CodegoryModel to frontend

---
+++ client/src/models/codegory.js
@@ -0,0 +1,59 @@
+const url = "http://localhost:4000/api/v1/codegory";
+
+class CodegoryModel {
+  static all() {
+    return fetch(url)
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CodegoryModel.all: ", err);
+        return { codegories: [] };
+      });
+  }
+
+  static getCodegory(id) {
+    return fetch(`${url}/${id}`)
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CodegoryModel.getCodegory: ", err);
+        return { codegory: {} };
+      });
+  }
+
+  static create(newCodegory) {
+    return fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newCodegory),
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CodegoryModel.create: ", err);
+        return { codegory: {} };
+      });
+  }
+  // but will this erase all the other info? or just the info specified?
+  static update(id, updatedCodegory) {
+    return fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedCodegory),
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CodegoryModel.update", err);
+        return { codegory: {} };
+      });
+  }
+  static delete(id) {
+    return fetch(`${url}/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CodegoryModel.delete", err);
+        return { codegory: {} };
+      });
+  }
+}
+
+export default CodegoryModel;
