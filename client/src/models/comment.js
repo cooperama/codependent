718bbd537ae19cf551738c346a6ef5b993258b45
cooperama <coope133@gmail.com>
2020-11-30 16:42:30 -0800

    Adds CommentModel to frontend

---
+++ client/src/models/comment.js
@@ -0,0 +1,59 @@
+const url = "http://localhost:4000/api/v1/comment";
+
+class CommentModel {
+  static all() {
+    return fetch(url)
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CommentModel.all: ", err);
+        return { comments: [] };
+      });
+  }
+
+  static Comment(id) {
+    return fetch(`${url}/${id}`)
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CommentModel.Comment: ", err);
+        return { comment: {} };
+      });
+  }
+
+  static create(Comment) {
+    return fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(Comment),
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CommentModel.create: ", err);
+        return { comment: {} };
+      });
+  }
+  // but will this erase all the other info? or just the info specified?
+  static update(id, Comment) {
+    return fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(Comment),
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CommentModel.update", err);
+        return { comment: {} };
+      });
+  }
+  static delete(id) {
+    return fetch(`${url}/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("Error fetching data in CommentModel.delete", err);
+        return { comment: {} };
+      });
+  }
+}
+
+export default CommentModel;
