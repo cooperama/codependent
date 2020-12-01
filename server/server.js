ce1f7da4233608761118c4dff0b3dd89b854d766
cooperama <coope133@gmail.com>
2020-12-01 10:34:55 -0800

    Sets up auth0 on server side

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { auth, requiresAuth } = require("express-openid-connect");
 
 const routes = require("./routes");
 
@@ -15,6 +16,18 @@ const corsOptions = {
 // middleware
 app.use(express.json());
 app.use(cors(corsOptions));
+// auth middleware
+
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+  })
+);
 
 // set up routes
 // MVP
@@ -28,7 +41,12 @@ app.use("/api/v1/paired", routes.paired);
 
 // Server home
 app.get("/", (req, res) => {
-  res.send("co[de]pendent lol");
+  // res.send("co[de]pendent lol");
+  res.send(req.oidc.isAuthenticated() ? "Logged In" : "Logged Out");
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 app.listen(PORT, () => console.log("Listening on port ", PORT));
