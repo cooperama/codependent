78ca8a7239d98dac19c3b1f19082ef4a7c9255c0
cooperama <coope133@gmail.com>
2020-12-11 17:30:11 -0800

    Styles error message

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,35 +1,30 @@
 import React, { useState, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import UserModel from "../../models/user";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamation } from "@fortawesome/free-solid-svg-icons";
 
 export default function Signup({ userState, setUserState }) {
-  const errorRef = useRef();
+  const errorMessageRef = useRef();
+  const errorBoxRef = useRef();
+  const passwordRef = useRef();
+  const password2Ref = useRef();
+  const history = useHistory();
+
   const [username, setUsername] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [password2, setPassword2] = useState();
-  //
-  const [newUser, setNewUser] = useState();
-  //
 
-  const handleInputChange = (e) => {
-    console.log("input change");
-    const property = e.target.name;
-    setNewUser({
-      ...newUser,
-      property: e.target.value,
-    });
-    console.log("new user: ", newUser);
-  };
-  //
-  const history = useHistory();
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
-
-    console.log(e.target.name);
+    errorBoxRef.current.style.display = "none";
+    errorMessageRef.current.innerText = "";
   };
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
+    errorBoxRef.current.style.display = "none";
+    errorMessageRef.current.innerText = "";
   };
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
@@ -37,7 +32,6 @@ export default function Signup({ userState, setUserState }) {
   const handlePassword2Change = (e) => {
     setPassword2(e.target.value);
   };
-  //
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -46,13 +40,12 @@ export default function Signup({ userState, setUserState }) {
       email,
       password,
     };
-
     UserModel.create(newUser).then((data) => {
-      console.log("creating user in Signup.js : ", data);
       if (data.error) {
-        errorRef.current.innerText = `${data.error} \n try again`;
-        setPassword("");
-        setPassword2("");
+        errorBoxRef.current.style.display = "block";
+        errorMessageRef.current.innerText = `${data.error} \n please try again`;
+        passwordRef.current.value = "";
+        password2Ref.current.value = "";
       } else {
         localStorage.setItem("uid", data.signedJwt);
         UserModel.getUser().then((data) => {
@@ -64,12 +57,11 @@ export default function Signup({ userState, setUserState }) {
   };
   return (
     <div className="signup-container">
-      <p ref={errorRef} className="error-message"></p>
       <form onSubmit={handleSubmit} className="signup-form">
         <div className="form-group">
           <input
             placeholder="username"
-            onChange={(handleInputChange, handleUsernameChange)}
+            onChange={handleUsernameChange}
             type="text"
             name="username"
             id="username"
@@ -78,7 +70,7 @@ export default function Signup({ userState, setUserState }) {
         <div className="form-group">
           <input
             placeholder="email"
-            onChange={(handleInputChange, handleEmailChange)}
+            onChange={handleEmailChange}
             type="email"
             name="email"
             id="email"
@@ -86,8 +78,9 @@ export default function Signup({ userState, setUserState }) {
         </div>
         <div className="form-group">
           <input
+            ref={passwordRef}
             placeholder="password"
-            onChange={(handleInputChange, handlePasswordChange)}
+            onChange={handlePasswordChange}
             type="password"
             name="password"
             id="password"
@@ -95,8 +88,9 @@ export default function Signup({ userState, setUserState }) {
         </div>
         <div className="form-group">
           <input
+            ref={password2Ref}
             placeholder="confirm password"
-            onChange={(handleInputChange, handlePassword2Change)}
+            onChange={handlePassword2Change}
             type="password"
             name="password2"
             id="password2"
@@ -104,6 +98,12 @@ export default function Signup({ userState, setUserState }) {
         </div>
         <input placeholder="" type="submit" value="sign up" />
       </form>
+      <div className="error-div" ref={errorBoxRef}>
+        <span className="error-icon">
+          <FontAwesomeIcon icon={faExclamation} />
+        </span>
+        <p ref={errorMessageRef} className="error-message"></p>
+      </div>
     </div>
   );
 }
